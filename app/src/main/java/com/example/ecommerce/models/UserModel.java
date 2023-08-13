808f5e6af55b9c71fc35202816489e1da58432c0
Neha Pandey <nehadotnet>
2023-08-13 14:05:03 +0530

    Add user registration and save user data into firebase firestore

---
+++ app/src/main/java/com/example/ecommerce/models/UserModel.java
@@ -0,0 +1,50 @@
+package com.example.ecommerce.models;
+
+public class UserModel {
+    private String userId;
+    private String userName;
+    private String email;
+    private String password;
+
+    public UserModel() {
+    }
+
+    public UserModel(String userId, String userName, String email, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/com/example/ecommerce/view/fragments/SignUpFragment.java
+++ app/src/main/java/com/example/ecommerce/view/fragments/SignUpFragment.java
@@ -4,15 +4,28 @@ import static com.example.ecommerce.utils.Utils.isValidEmail;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.ecommerce.R;
+import com.example.ecommerce.models.UserModel;
+import com.example.ecommerce.utils.Utils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpFragment extends Fragment {
     TextView txtUserName, txtEmail, txtPassword;
@@ -36,20 +49,37 @@ public class SignUpFragment extends Fragment {
     }
 
     private void setUI() {
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkValidation()) {
-
-                }
+        btnSignUp.setOnClickListener(v -> {
+            String userName = txtUserName.getText().toString();
+            String email = txtEmail.getText().toString();
+            String password = txtPassword.getText().toString();
+            if (checkValidation(userName, email, password)) {
+                doSignUp(userName, email, password);
             }
         });
     }
 
-    private boolean checkValidation() {
-        String userName = txtUserName.getText().toString();
-        String email = txtEmail.getText().toString();
-        String password = txtPassword.getText().toString();
+    private void doSignUp(String userName, String email, String password) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            UserModel userModel = new UserModel(firebaseUser.getUid(), userName, email, password);
+                            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                            firestore.collection("users").document().set(userModel)
+                                    .addOnSuccessListener(documentReference -> Log.e("TAG", "onSuccess: "))
+                                    .addOnFailureListener(e -> Log.e("TAG", "onFailure: " ));
+
+                            Utils.showMessage(getContext(), getString(R.string.user_registered_successfully));
+                        }
+                    }
+                });
+    }
+
+    private boolean checkValidation(String userName, String email, String password) {
         if (userName.length() == 0) {
             txtUserName.setError(getString(R.string.enter_username));
             txtUserName.requestFocus();
@@ -60,7 +90,7 @@ public class SignUpFragment extends Fragment {
             txtEmail.requestFocus();
             return false;
         }
-        if (isValidEmail(email)) {
+        if (!isValidEmail(email)) {
             txtEmail.setError(getString(R.string.enter_valid_email_id));
             txtEmail.requestFocus();
             return false;
